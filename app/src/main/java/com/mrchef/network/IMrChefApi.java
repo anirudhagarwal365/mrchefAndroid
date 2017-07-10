09dddbb6958c1c24229813c92b9230654a4292a4
Anirudh Agarwal <anirudh.agarwal@gdn-commerce.com>
2017-07-10 17:18:23 +0530

    food selection

--- app/src/main/java/com/mrchef/network/IMrChefApi.java
+++ app/src/main/java/com/mrchef/network/IMrChefApi.java
@@ -1,5 +1,7 @@
 package com.mrchef.network;
 
+import com.mrchef.food_detail.models.CatererDetailsAndMenu;
+import com.mrchef.food_detail.models.SubmitMenuDetail;
 import com.mrchef.login.User;
 import com.mrchef.model.consumption_model.FoodConsumptionDetail;
 
@@ -8,6 +10,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by vinod on 07/07/17.
@@ -17,4 +20,10 @@ public interface IMrChefApi {
   // just an demo api do not use it
   @PUT("addUser")
   Call<Boolean> loginRequest(@Body User user);
+
+  @GET("menuItems")
+  Call<CatererDetailsAndMenu> menuRequest(@Query("date") String date);
+
+  @GET("/submit/menu")
+  Call<Boolean> submitMenu(@Body SubmitMenuDetail submitMenuDetail);
 }
