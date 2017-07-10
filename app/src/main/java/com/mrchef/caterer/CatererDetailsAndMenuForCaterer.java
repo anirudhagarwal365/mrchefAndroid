8f21ab880d59d0808a8b14ace099032cf67697e0
Anirudh Agarwal <anirudh.agarwal@gdn-commerce.com>
2017-07-10 17:42:04 +0530

    commit

--- app/src/main/java/com/mrchef/caterer/CatererDetailsAndMenuForCaterer.java
+++ app/src/main/java/com/mrchef/caterer/CatererDetailsAndMenuForCaterer.java
@@ -6,13 +6,13 @@ import java.util.List;
  * Created by PrashantKumar on 7/10/17.
  */
 
-public class CatererDetailsAndMenu {
+public class CatererDetailsAndMenuForCaterer {
 
   private String vendorName;
   private String date;
   private List<FoodItem> foodItems;
 
-  public CatererDetailsAndMenu(String vendorName, String date, List<FoodItem> foodItems) {
+  public CatererDetailsAndMenuForCaterer(String vendorName, String date, List<FoodItem> foodItems) {
     this.vendorName = vendorName;
     this.date = date;
     this.foodItems = foodItems;
